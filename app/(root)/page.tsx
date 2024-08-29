4ff3f3f6c2cc02de39965e43d7b84a894ca25c1f
Kirill Krymov <kkrymov.asc@gmail.com>
2024-08-29 12:17:15 +0300

    Complete Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import RightSideBar from "@/components/RightSideBar";
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Kirill", lastName: "Krymov", email:"kkrymov.x@gmail.com" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext = "Access and manage your account transactions efficiently"
+          />
+          <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={3921.92}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSideBar
+      user={loggedIn}
+      transactions={[]}
+      banks={[{currentBalance: 3921.92 },{currentBalance: 5641.92 }]}
+      />
+    </section>
+  );
+};
+
+export default Home;
